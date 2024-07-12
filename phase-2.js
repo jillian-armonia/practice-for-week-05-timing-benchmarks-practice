1b805bd49fa6edb447568880351bd09f4716c8f8
Jillian Armonia <jillian.marice@gmail.com>
2024-07-12 11:25:49 +0900

    completed phase 2

--- phase-2.js
+++ phase-2.js
@@ -2,14 +2,30 @@ const [addNums, addManyNums] = require("./phase-1");
 
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
-  // Fill this in
+  let result = [];
+  let start = 0;
+
+  for (let i = 0; i < 10; i++){
+    result.push(addNums(start += increment))
+  }
+
+  return result;
 
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
-  // Fill this in
+  let result = [];
+  let start = 0;
+
+  for (let i = 0; i < 10; i++){
+    result.push(addManyNums(start += increment))
+  }
+
+  return result;
 
 }
 
 module.exports = [addNums10, addManyNums10];
+
+console.log(addManyNums10(1))
