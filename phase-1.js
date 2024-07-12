381d02d3179061fc5577ba2d41d5ef3d6668e8a4
Jillian Armonia <jillian.marice@gmail.com>
2024-07-12 11:12:00 +0900

    completed phase 1

--- phase-1.js
+++ phase-1.js
@@ -1,16 +1,30 @@
 // Adds up positive integers from 1-n
 function addNums(n) {
-  // Fill this in
+  let sum = 0;
+
+  for (let i = sum; i <= n; i++){
+    sum += i;
+  }
+
+  return sum;
 
 }
 
 
 // Adds up values of addNums(1) through addNums(n)
 function addManyNums(n) {
-  // Fill this in
+  let total = 0;
+
+  for (let i = total; i <= n; i++){
+    total += addNums(i);
+  }
+
+  return total;
 
 }
 
 
 
 module.exports = [addNums, addManyNums];
+
+console.log(addManyNums(10))
